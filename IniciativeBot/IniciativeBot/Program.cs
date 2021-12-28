b03a774cd62241d97b67628d98cf140a6e59c816
Andrzej Litkowski <lank891@gmail.com>
2021-12-28 19:50:56 +0100

    Fixed Program.cs

--- IniciativeBot/IniciativeBot/Program.cs
+++ IniciativeBot/IniciativeBot/Program.cs
@@ -1,12 +1,1 @@
-﻿
-using IniciativeBot.Rolling;
-
-RNG.SetSeed(15);
-
-for (int i = 0; i < 20; i++)
-{
-    Console.Write(RNG.RollDice(20));
-    Console.Write(", ");
-}
-
-RNG.SetSeed()
+﻿Console.WriteLine("Hello world");
