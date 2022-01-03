b00dcf9eadeaa57a04e546711210fe75eda691ef
Xerisu <k4s14.kk@gmail.com>
2022-01-03 22:29:06 +0100

    Adding disadvantage

---
+++ InitiativeBot/InitiativeBot/Rolling/Modifiers/DisadvantageRollModifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitiativeBot.Rolling.Modifiers
+{
+    /// <summary>
+    /// Rolling 2 dice and getting lower result.
+    /// </summary>
+    public class DisadvantageRollModifier : IRollModifier
+    {
+        private IRoll _roll;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="roll">Base roll.</param>
+        public DisadvantageRollModifier(IRoll roll)
+        {
+            _roll = roll;
+        }
+
+        /// <inheritdoc/>
+        public int RollDice()
+        {
+            int roll1 = _roll.RollDice();
+            int roll2 = _roll.RollDice();
+            return Math.Min(roll1, roll2);
+
+        }
+        public override string ToString()
+        {
+            return $"dis({_roll})";
+        }
+    }
+
+}
