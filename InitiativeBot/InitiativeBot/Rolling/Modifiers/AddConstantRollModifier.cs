a5531f1dfeba2bddd733ccf0144f1e15cbb19f39
Xerisu <k4s14.kk@gmail.com>
2022-01-03 21:26:24 +0100

    Adding numbers to a roll

---
+++ InitiativeBot/InitiativeBot/Rolling/Modifiers/AddConstantRollModifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitiativeBot.Rolling.Modifiers
+{
+    /// <summary>
+    /// Adds number to a roll.
+    /// </summary>
+    public class AddConstantRollModifier : IRollModifier
+    {
+        private IRoll _roll;
+        private int _constant;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="roll">Base roll.</param>
+        /// <param name="constant">Number added to a roll.</param>
+        public AddConstantRollModifier(IRoll roll, int constant)
+        {
+            _roll = roll;
+            _constant = constant;
+        }
+
+
+        /// <inheritdoc/>
+        public int RollDice()
+        {
+            return _roll.RollDice() + _constant;
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString() + _constant.ToString("+0;-#");
+        }
+    }
+}
