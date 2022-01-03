99a422e2912060d259bf2d5934714db13b301651
Xerisu <k4s14.kk@gmail.com>
2022-01-03 22:58:59 +0100

    Adding roll multiplier

---
+++ InitiativeBot/InitiativeBot/Rolling/Modifiers/MultiplyingRollModifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitiativeBot.Rolling.Modifiers
+{
+    /// <summary>
+    /// Multiplying a roll.
+    /// </summary>
+    public class MultiplyingRollModifier : IRollModifier
+    {
+        private readonly IRoll _roll;
+        private readonly int _multiplier;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="roll">Base roll.</param>
+        /// <param name="multiplier">Roll multiplier.</param>
+        public MultiplyingRollModifier(IRoll roll, int multiplier)
+        {
+            _roll = roll;
+            _multiplier = multiplier;
+        }
+
+        /// <inheritdoc/>
+        public int RollDice()
+        {
+            return _multiplier * _roll.RollDice();
+        }
+
+        public override string ToString()
+        {
+            return _multiplier.ToString() + _roll.ToString();
+        }
+    }
+
+   
+}
