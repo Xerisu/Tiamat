d2fdbbb8adb997cb9ffcc3e45c391733ef70ec27
Andrzej Litkowski <lank891@gmail.com>
2022-01-05 09:07:37 +0100

    Added function to RNG for easier testing

--- InitiativeBot/InitiativeBot/Rolling/RNG.cs
+++ InitiativeBot/InitiativeBot/Rolling/RNG.cs
@@ -25,14 +25,34 @@ namespace InitiativeBot.Rolling
         /// <summary>
         /// Rolls a dice.
         /// </summary>
-        /// <param name="dice">Number of sides on a die, should be >= 1.</param>
+        /// <param name="dice">Number of sides on a die</param>
         /// <returns>Result of a roll</returns>
         /// <exception cref="ArgumentOutOfRangeException">If argument is 0 or less.</exception>
         public static int RollDice(int dice)
         {
             if (dice < 1)
-                throw new ArgumentOutOfRangeException($"Dice has to be greater than 0. Provided {dice}");
+                throw new ArgumentOutOfRangeException(nameof(dice), dice, $"Dice has to be greater than 0. Provided {dice}" );
             return random.Next(dice) + 1;
+        }
+
+        /// <summary>
+        /// Creates new stream of results from rolling a dice with given seed.
+        /// <para>Should be consistent with setting seed and rolling a dice several times.</para>
+        /// <para>Used for testing.</para>
+        /// </summary>
+        /// <param name="seed">Seed to use</param>
+        /// <param name="dice">Number of sides on a die, should be >= 1.</param>
+        /// <returns>Infinite IEnumerable with next rolls</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If argument is 0 or less.</exception>
+        public static IEnumerable<int> GetStreamOfRandomNumbersFromSeed(int seed, int dice)
+        {
+            if (dice < 1)
+                throw new ArgumentOutOfRangeException(nameof(dice), dice, $"Dice has to be greater than 0. Provided {dice}");
+            Random r = new(seed);
+            while(true)
+            {
+                yield return r.Next(dice) + 1;
+            }
         }
     }
 }
--- InitiativeBot/InitiativeBotTests/Rolling/RNGTests.cs
+++ InitiativeBot/InitiativeBotTests/Rolling/RNGTests.cs
@@ -12,15 +12,20 @@ namespace InitiativeBotTests.Rolling
 {
     public class RNGTests
     {
+        private const int _testSeed = 15;
+        private const int _testDice = 20;
+        private const int _numerOfTestRolls = 20;
+        private static readonly int[] _twentyExpectedValuesForSeed15Dice20 = new int[] { 12, 5, 6, 16, 20, 14, 13, 20, 17, 17, 7, 4, 19, 6, 10, 8, 4, 6, 2, 3 };
+
+
         [Fact]
         public void RollDice_Should_ReturnRandomInteger()
         {
-            int[] expectedValues = new int[] { 12, 5, 6, 16, 20, 14, 13, 20, 17, 17, 7, 4, 19, 6, 10, 8, 4, 6, 2, 3 };
-            RNG.SetSeed(15);
-            for (int i = 0; i < 20; i++)
+            RNG.SetSeed(_testSeed);
+            for (int i = 0; i < _numerOfTestRolls; i++)
             {
-                int roll = RNG.RollDice(20);
-                Assert.Equal(expectedValues[i], roll);
+                int roll = RNG.RollDice(_testDice);
+                Assert.Equal(_twentyExpectedValuesForSeed15Dice20[i], roll);
             }
         }
         
@@ -31,6 +36,30 @@ namespace InitiativeBotTests.Rolling
         public void RollDice_Should_ThrowException_When_DiceIsLowerThan1(int dice)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => RNG.RollDice(dice));
+        }
+
+        [Fact]
+        public void GetStreamOfRandomNumbersFromSeed_Should_ReturnExpectedValuesForSeed()
+        {
+            var diceRolls = RNG.GetStreamOfRandomNumbersFromSeed(_testSeed, _testDice);
+            var rolls = diceRolls.Take(_numerOfTestRolls).ToArray();
+            for(int i = 0; i < _numerOfTestRolls; i++)
+            {
+                Assert.Equal(_twentyExpectedValuesForSeed15Dice20[i], rolls[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-15)]
+        public void GetStreamOfRandomNumbersFromSeed_Should_ThrowException_When_DiceIsLowerThan1(int dice)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var diceRolls = RNG.GetStreamOfRandomNumbersFromSeed(0, dice);
+                var roll = diceRolls.Take(1).ToArray()[0];
+            });
         }
     }
 }
