58d25cd886c6f614a42a5262e16c54509562cee6
Andrzej Litkowski <lank891@gmail.com>
2022-01-08 13:55:49 +0100

    Tests of mock rng

--- InitiativeBot/InitiativeBotTests/Rolling/MockRNG.cs
+++ InitiativeBot/InitiativeBotTests/Rolling/MockRNG.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace InitiativeBotTests.Rolling
 {
@@ -35,6 +36,37 @@ namespace InitiativeBotTests.Rolling
         public int GetNthRoll(int dice, int n)
         {
             return values[(n-1)%numberOfValues]%dice + 1;
+        }
+    }
+
+    public class MockRNGTests
+    {
+        [Fact]
+        public void RollDice_Should_ReturnProperInteger()
+        {
+            const int numberOfValues = 30;
+            const int dice = 101;
+
+            int[] expectedValues = new int[numberOfValues] { 41, 101, 46, 85, 67, 25, 45, 26, 79, 49, 56, 68, 38, 81, 51, 39, 69, 72, 95, 28, 41, 101, 46, 85, 67, 25, 45, 26, 79, 49 };
+
+            var rng = new MockRNG();
+            for(int i = 0; i < 30; i++)
+            {
+                int roll = rng.RollDice(dice);
+                Assert.Equal(expectedValues[i], roll);
+            }
+        }
+
+        [Theory]
+        [InlineData(20, 1, 1)]
+        [InlineData(20, 2, 1)]
+        [InlineData(12, 3, 10)]
+        [InlineData(8, 5, 3)]
+        public void GetNthRollShould_ReturnProperValue(int dice, int n, int expectedValue)
+        {
+            var rng = new MockRNG();
+            int result = rng.GetNthRoll(dice, n);
+            Assert.Equal(expectedValue, result);
         }
     }
 }
