3dc526a7cfcdc8c2105f7c7688e0b69ecb61db30
Xerisu <k4s14.kk@gmail.com>
2022-01-15 23:54:23 +0100

    Added help message

--- InitiativeBot/Discord/Constants.cs
+++ InitiativeBot/Discord/Constants.cs
@@ -21,7 +21,26 @@ namespace Discord
             {
                 public const string CommandName = "help";
                 public const string CommandDescription = "Shows help about Tiamat.";
-                public const string ResponseMessage = "Tiamat usage:\nTo configure channel use commad `/setup`.\nThen just use new channel lol.";
+                public const string ResponseMessage =
+@"**/Help Command**
+
+**/create-channel** - Adds the channel  `#tiamat-initiative-list`  in case it was removed or its name got changed
+
+**/join `player-name` `modifiers`** - Adds player to the initiative queue, with an `inactive` state (inactive players are skipped in the current round, and are hidden with ||spoilers||. Player-name can have spaces and special characters. To add modifiers, confirm typing name by pressing `Tab` , choose `modifiers` from the list (or press `Tab` twice) and then type modifiers of your initiative roll
+*possible modifiers:*
+`adv` - adds advantage to a roll (2d20, drop lowest)
+`dis` - adds disadvantage to a roll (2d20, drop highest)
+`+<number>` - adds a constant number to a roll, ex. +8
+`-<number>` - subtracts a constant number from a roll, ex. -1
+`+d<number>` -  adds a dice to a roll, ex. +d8
+`-d<number>` - subtracts a dice number from a roll, ex. -d10
+You can add or subtract multiple dices, ex. +2d4, -2d10
+
+When you add a player that already exists, it will remove the existing one and add him once again
+
+**/remove `player-name`** - removes player from the queue.
+
+**/clear** - clears the queue and resets the round counter (use it only when the battle finishes)";
             }
 
             public static class TiamatClear
